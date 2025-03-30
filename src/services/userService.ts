ae1cfd47b2fd20ce1ce420e475a89f373ed58a23
fduTristin <3572887883@qq.com>
2025-03-31 00:22:33 +0800

    feat(api): modify request route and headers
    - baseURL: changed from `apifox` to `localhost`
    - request header: get `token` from `useAuthStore`

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,11 +1,13 @@
 import { api } from './api'
 import type { User } from '@/types/user'
+import { useAuthStore } from '@/stores/auth'
 
 export const userService = {
   // 获取所有项目
   getAll: async () => {
     console.log('Fetching all users...')
-    const token = ''; 
+    const token = useAuthStore().token;
+    console.log(`Token: ${token}`) 
     return await api.get<User[]>('/admin/users', {
       headers: {
         'Authorization': `Bearer ${token}`,
